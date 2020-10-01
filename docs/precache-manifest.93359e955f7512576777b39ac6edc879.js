0b915acecbd26f514795cbe30a855a5aad345993
Alvaro Diez <alvarodr_97@hotmail.com>
2020-10-01 13:20:54 +0200

    docs agregar

---
+++ docs/precache-manifest.93359e955f7512576777b39ac6edc879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d22345878b02e1130460d465b66ae5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80fd8753ce43a50c3f0c",
+    "url": "/static/css/main.6232fa39.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80fd8753ce43a50c3f0c",
+    "url": "/static/js/main.5510258e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
